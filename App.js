4bbd5600cf5db85174f7fa117cbfb4e1a0ec97cf
suopoika <henrik.junttila@gmail.com>
2024-01-09 14:20:24 +0200

    lisää tiedostot

--- App.js
+++ App.js
@@ -1,13 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [age, setAge] = useState("0")
+  const [limits, setLimits] = useState(0)
+
+  function calculate() {
+    const result1 = (220 - age) * 0.65
+    const result2 = (220 - age) * 0.85
+    const result = result1 + " - " + result2
+    setLimits(result)
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.field}>Age</Text>
+
+      <TextInput keyboardType='decimal-pad'value = {age}
+      onChangeText={text => setAge(text)} />
+
+      <Text style={styles.field}>Limits</Text>
+      <Text style={styles.field}>{limits}</Text>
+
+      <Button onPress={calculate} title="Calculate"></Button>
       <StatusBar style="auto" />
     </View>
   );
+  
 }
 
 const styles = StyleSheet.create({
@@ -17,4 +38,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  field: {
+    marginBottom: 8,
+  }
 });
+
